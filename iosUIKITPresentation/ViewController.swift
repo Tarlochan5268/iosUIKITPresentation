02297b449a4c3d921a707c777bd17a6e24c14bf5
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-07-09 19:24:42 -0400

    Initial Commit

---
+++ iosUIKITPresentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosUIKITPresentation
+//
+//  Created by MacStudent on 2019-07-09.
+//  Copyright © 2019 tarlochan5268. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
