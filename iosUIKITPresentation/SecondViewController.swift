0f2299ede2a15fe1bbd9c79cf839761efa84ab0a
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-07-09 20:48:39 -0400

    commit

---
+++ iosUIKITPresentation/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  iosUIKITPresentation
+//
+//  Created by MacStudent on 2019-07-09.
+//  Copyright © 2019 tarlochan5268. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iosUIKITPresentation/ViewController.swift
+++ iosUIKITPresentation/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource
+{
+    
+    @IBAction func actionAdd(_ sender: Any)
+    {
+        let alertControl = UIAlertController(title: "Action Sheet", message: "Go To Next Screen", preferredStyle: .actionSheet)
+        
+        let actionOk = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertControl.addAction(actionOk)
+        alertControl.addAction(actionCancel)
+        
+        self .present(alertControl , animated: true , completion: nil)
+        
+        
+    }
+    
+    var groupArray : [String] = ["1","2","3","4","5","6","7","8","9"]
+    @IBOutlet weak var PickerGroup: UIPickerView!
+    override func viewDidLoad()
+    {
+        self.PickerGroup.delegate = self
+        self.PickerGroup.dataSource = self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return groupArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return self.groupArray[row]
+    }
 
 }
 
+
+ 
